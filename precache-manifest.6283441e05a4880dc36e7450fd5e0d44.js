5bff1341640f40c60f062f3c46460ca6462edce5
rachetm <rmudnur@gmail.com>
2019-12-27 23:06:46 +0530

    Updates

---
+++ precache-manifest.6283441e05a4880dc36e7450fd5e0d44.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ee05727e649628be88e572c105c5bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a118de5d846312500bec",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "15305c0b0dd2fd5a263e",
+    "url": "/static/css/main.122ff22f.chunk.css"
+  },
+  {
+    "revision": "a118de5d846312500bec",
+    "url": "/static/js/2.95f59642.chunk.js"
+  },
+  {
+    "revision": "15305c0b0dd2fd5a263e",
+    "url": "/static/js/main.1c04bb69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0bb34fe6e92e2a4213742a9d1337ad3c",
+    "url": "/static/media/Black_Hackbout.0bb34fe6.png"
+  },
+  {
+    "revision": "1f9c79a761f65f9017251477e9089322",
+    "url": "/static/media/Hackbout_T.1f9c79a7.png"
+  },
+  {
+    "revision": "8e5f3395e44ce1ee351172987e9a56d8",
+    "url": "/static/media/White_Hackbout.8e5f3395.png"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "29892f439f530d9ee9960f05b67dc67b",
+    "url": "/static/media/github-logo.29892f43.png"
+  },
+  {
+    "revision": "8e1cb32867eca4b0019578c4b9cd2325",
+    "url": "/static/media/hack-club.8e1cb328.svg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/static/media/ryan.f217f346.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hackbout/precache-manifest.84cb0844e96529f8dc0de0330e9b99bf.js"
+  "/precache-manifest.6283441e05a4880dc36e7450fd5e0d44.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackbout/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
