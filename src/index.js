5964e682e29767de0d1d2401405e867909540b6c
Anshul Rai <41705919+auralshin@users.noreply.github.com>
2020-01-03 23:39:40 +0530

    scroll to top button added (#1)
    Scroll to top button added

--- src/index.js
+++ src/index.js
@@ -57,3 +57,66 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+const Scrollup = (prop) => {
+  return (
+    <div class="scrollup">
+    </div>
+  );
+};
+
+class GoTop extends React.Component {
+  state = {
+       intervalId: 0,
+       thePosition: false
+   };
+
+  componentDidMount() {
+      document.addEventListener("scroll", () => {
+          if (window.scrollY > 170) {
+              this.setState({ thePosition: true })
+          } else {
+              this.setState({ thePosition: false })
+          }
+      });
+      window.scrollTo(0, 0);
+  }
+  
+  onScrollStep = () => {
+      if (window.pageYOffset === 0){
+          clearInterval(this.state.intervalId);
+      }
+      window.scroll(0, window.pageYOffset - this.props.scrollStepInPx);
+  }
+
+  scrollToTop = () => {
+      let intervalId = setInterval(this.onScrollStep, this.props.delayInMs);
+      this.setState({ intervalId: intervalId });
+  }
+
+  renderGoTopIcon = () => {
+      if (this.state.thePosition){
+          return (
+              <div className="go-top" onClick={this.scrollToTop}>
+                  Go To Top
+              </div>
+          )
+      }
+  }
+
+  render(){
+      return (
+          <React.Fragment>
+              {this.renderGoTopIcon()}
+          </React.Fragment>
+      )
+  }
+}
+
+const Scrollups = (
+<div>
+  <GoTop scrollStepInPx="50" delayInMs="30" />
+</div>
+);
+
+ReactDOM.render(Scrollups, document.querySelector('#app'));
