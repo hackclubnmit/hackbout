b972860a7e1a8934f2b9c2118bf0863314db5bd5
rachetm <rmudnur@gmail.com>
2020-01-04 03:44:43 +0530

    Removed /index that showed up in url

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Switch>
-        <Route path="/index" render={props => <Index {...props} />} />
+        <Route path="/" render={props => <Index {...props} />} />
         <Route
           path="/nucleo-icons"
           render={props => <NucleoIcons {...props} />}
@@ -32,8 +32,7 @@ ReactDOM.render(
           render={props => <ProfilePage {...props} />}
         />
         <Route path="/login-page" render={props => <LoginPage {...props} />} />
-        <Redirect to="/index" />
-        <Redirect from="/" to="/index" />
+        <Redirect to="/" />
       </Switch>
     </Switch>
   </BrowserRouter>,
