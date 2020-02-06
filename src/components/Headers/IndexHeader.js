637f38b7912ec3c27d9d90203d76a45359f207cc
rudyt7 <gallacticsoccer@gmail.com>
2020-02-06 20:49:40 +0530

    devfolio button added

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -13,6 +13,30 @@ function IndexHeader() {
   let pageHeader = React.createRef();
 
   React.useEffect(() => {
+    document.addEventListener("DOMContentLoaded", function() {
+      let devfolioOptions = {
+        buttonSelector: "#devfolio-apply-now",
+        key: "hackbout"
+      };
+
+      let script = document.createElement("script");
+      script.src = "https://apply.devfolio.co";
+      document.head.append(script);
+
+      script.onload = function() {
+        new Devfolio(devfolioOptions);
+      };
+
+      script.onerror = function() {
+        document
+          .querySelector(devfolioOptions.buttonSelector)
+          .addEventListener("click", function() {
+            window.location.href =
+              "https://devfolio.co/external-apply/" + devfolioOptions.key;
+          });
+      };
+    });
+
     const pathEls = document.querySelectorAll("path");
     for (var i = 0; i < pathEls.length; i++) {
       const pathEl = pathEls[i];
@@ -330,8 +354,8 @@ function IndexHeader() {
             </svg>
           </div>
         </div>
-        <Container className="">
-          <div className="">
+        <Container className="" style={{ textAlign: "center" }}>
+          <div className="text-center">
             <img
               className={styles.Logo}
               alt="Logo Here"
@@ -340,24 +364,44 @@ function IndexHeader() {
             <h4 className="" style={{ color: "black" }}>
               <strong>A Hack Club Hackathon</strong>
             </h4>
-            <Button size="lg" className={styles.button}>
-              <img src={dev} alt="devdolio" />
-              Apply With Devfolio
-            </Button>
+            <div
+              style={{
+                textAlign: "center",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: "10px auto"
+              }}
+            >
+              <button
+                id="devfolio-apply-now"
+                className={styles.devfolioApplyNow}
+                style={{
+                  textAlign: "center",
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              >
+                <svg
+                  className="logo"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#fff"
+                  viewBox="0 0 115.46 123.46"
+                  style={{ height: "24px", width: "24px", marginRight: "8px" }}
+                >
+                  <path d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.05 1.05.08 2.18.08 3.34z" />
+                  <path d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z" />
+                </svg>
+                Apply with Devfolio
+              </button>
+            </div>
           </div>
-          {/* <h2
-            className="category-absolute"
-            style={{ color: "black", "margin-top": "15px" }}
-          >
-            Registrations will open soon! Stay tuned!
-          </h2> */}
+
           <h2 className="" style={{ color: "black" }}>
             <strong>4th & 5th March</strong>
             <br></br>
             <strong>Nitte Meenakshi Institute of Technology, Bangalore.</strong>
           </h2>
           <br></br>
-          {/* <Button size="lg">Telegram</Button> */}
         </Container>
       </div>
     </>
@@ -365,3 +409,10 @@ function IndexHeader() {
 }
 
 export default IndexHeader;
+
+{
+  /* <Button size="lg" className={styles.button}>
+              <img src={dev} alt="devdolio" />
+              Apply With Devfolio
+            </Button> */
+}
